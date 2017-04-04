6e2d689910a1060ba3a89b1a411a930c7a778f7e
bird50 <siratis.w@gmail.com>
2017-04-04 16:43:49 +0700

    try by example passport

--- server/server.js
+++ server/server.js
@@ -1,15 +1,54 @@
+// Copyright IBM Corp. 2014,2016. All Rights Reserved.
+// Node module: loopback-example-passport
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
 'use strict';
-
+console.log('Hello server');
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-var cookieParser = require('cookie-parser');
 var app = module.exports = loopback();
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
-var session =require('express-session');
 
 
 
-// Setup the view engine (jade)
+
+// Passport configurators..
+var loopbackPassport = require('loopback-component-passport');
+var PassportConfigurator = loopbackPassport.PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
+/*
+ * body-parser is a piece of express middleware that
+ *   reads a form's input and stores it as a javascript
+ *   object accessible through `req.body`
+ *
+ */
+var bodyParser = require('body-parser');
+
+/**
+ * Flash messages for passport
+ *
+ * Setting the failureFlash option to true instructs Passport to flash an
+ * error message using the message given by the strategy's verify callback,
+ * if any. This is often the best approach, because the verify callback
+ * can make the most accurate determination of why authentication failed.
+ */
+var flash      = require('express-flash');
+
+// attempt to build the providers/passport config
+var config = {};
+try {
+  config = require('../providers.json');
+} catch (err) {
+  console.trace(err);
+  process.exit(1); // fatal
+}
+
+// -- Add your pre-processing middleware here --
+
+// Setup the view engine (nunjucks)
 var path = require('path');
 var consolidate = require('consolidate');
 
@@ -18,115 +57,131 @@ var consolidate = require('consolidate');
  app.set('view engine', 'html');
  app.set('views', path.join(__dirname, 'views'));
 
- // Bootstrap the application, configure models, datasources and middleware.
- // Sub-apps like REST API are mounted via boot scripts.
- boot(app, __dirname, function(err) {
-   if (err) throw err;
+// boot scripts mount components like REST API
+//boot(app, __dirname);
+boot(app, __dirname, function(err) {
+  if (err) throw err;
   
-   // start the server if `$ node server.js`
-   if (require.main === module){
-     var appModels = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role','Test3','Test','Test2','m04','myuser','RidOffice','RidAgency','flowto','flowtoUser','accessToken','userCredential','userIdentity'];
+  // start the server if `$ node server.js`
+  if (require.main === module){
+	  
+    var appModels = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role','Test3','Test','Test2','m04','myuser','RidOffice','RidAgency','flowto','flowtoUser','accessToken','userCredential','userIdentity'];
 
- var ds = app.dataSources.db;
+var ds = app.dataSources.db;
 
-// ds.isActual(appModels, function(err, actual) {
-  // if (!actual) {
-     ds.autoupdate(appModels, function(err) {
-       if (err) throw (err);
- 	});
-//   }//if (!actual) {
-// }); //ds.isActual
-   }//if (require.main === module)
-});//boot
-
-
-
-
-app.get('/test', function(req, res, next) {
-  res.render('_test.html', {
-    user: req.user,
-    url: req.url,
-  });
+ds.isActual(appModels, function(err, actual) {
+	//console.log('asdfasdfasdffd');
+  //if (!actual) {
+	
+    ds.autoupdate(appModels, function(err) {
+      if (err) throw (err);
+    });
+  //}
+});
+}
 });
 
-// Passport configurators..
-var loopbackPassport = require('loopback-component-passport');
-var PassportConfigurator = loopbackPassport.PassportConfigurator;
-var passportConfigurator = new PassportConfigurator(app);
-//app.use(loopback.session({ secret: 'keyboard cat' }));
-
-var config = {};
-try {
- config = require('../providers.json');
-} catch(err) {
- console.error('Please configure your passport strategy in `providers.json`.');
- console.error('Copy `providers.json.template` to `providers.json` and replace the clientID/clientSecret values with your own.');
- process.exit(1);
-}
+// to support JSON-encoded bodies
+app.middleware('parse', bodyParser.json());
+// to support URL-encoded bodies
+app.middleware('parse', bodyParser.urlencoded({
+  extended: true,
+}));
 
 // The access token is only available after boot
 app.middleware('auth', loopback.token({
   model: app.models.accessToken,
 }));
 
-
-// Initialize passport
+app.middleware('session:before', cookieParser(app.get('cookieSecret')));
+app.middleware('session', session({
+  secret: 'kitty',
+  saveUninitialized: true,
+  resave: true,
+}));
 passportConfigurator.init();
 
+// We need flash messages to see passport errors
+app.use(flash());
 
-// Set up related models
 passportConfigurator.setupModels({
   userModel: app.models.flowtoUser,
   userIdentityModel: app.models.userIdentity,
-  userCredentialModel: app.models.userCredential
+  userCredentialModel: app.models.userCredential,
 });
-// Configure passport strategies for third party auth providers
-for(var s in config) {
- var c = config[s];
- c.session = c.session !== false;
- passportConfigurator.configureProvider(s, c);
+for (var s in config) {
+  var c = config[s];
+  c.session = c.session !== false;
+  passportConfigurator.configureProvider(s, c);
 }
-
-
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 
+app.get('/', function(req, res, next) {
+  res.render('pages/index', {user:
+    req.user,
+    url: req.url,
+  });
+});
 
-//app.get('/auth/account',ensureLoggedIn('/login'), function(req, res, next) {
-app.get('/auth/account', function(req, res, next) {
-	
-	console.log('cookie token:'+req.headers.cookie);
-	var tok_model=app.models.accessToken;
-	tok_model.findById(req.headers.cookie.access_token,{include:"flowtoUser"},function(err,instance_tok){
-		console.log('ins:');
-		console.log(instance_tok);
-	});
-	
-	console.log(req.url);
-  res.render('loginfinish.html', {
+app.get('/auth/account', ensureLoggedIn('/login'), function(req, res, next) {
+  res.render('pages/loginProfiles', {
     user: req.user,
     url: req.url,
   });
 });
-app.get('/auth/logout', function(req, res, next) {
-  req.logout();
-  res.redirect('/');
+
+app.get('/local', function(req, res, next) {
+  res.render('pages/local', {
+    user: req.user,
+    url: req.url,
+  });
+});
+
+app.get('/signup', function(req, res, next) {
+  res.render('pages/signup', {
+    user: req.user,
+    url: req.url,
+  });
+});
+
+app.post('/signup', function(req, res, next) {
+  var User = app.models.user;
+
+  var newUser = {};
+  newUser.email = req.body.email.toLowerCase();
+  newUser.username = req.body.username.trim();
+  newUser.password = req.body.password;
+
+  User.create(newUser, function(err, user) {
+    if (err) {
+      req.flash('error', err.message);
+      return res.redirect('back');
+    } else {
+      // Passport exposes a login() function on req (also aliased as logIn())
+      // that can be used to establish a login session. This function is
+      // primarily used when users sign up, during which req.login() can
+      // be invoked to log in the newly registered user.
+      req.login(user, function(err) {
+        if (err) {
+          req.flash('error', err.message);
+          return res.redirect('back');
+        }
+        return res.redirect('/auth/account');
+      });
+    }
+  });
 });
 
 app.get('/login', function(req, res, next) {
-	console.log(req.user);
-	console.log(req.url);
-    res.render('loginfinish.html', {
-      user: req.user,
-      url: req.url,
-    });
+  res.render('pages/login', {
+    user: req.user,
+    url: req.url,
+  });
 });
-app.get('/login.html', function(req, res, next) {
-	//console.log(req);
-	//console.log(req.url);
-    res.render('loginfinish.html', {
-      user: req.user,
-      url: req.url,
-    });
+
+app.get('/auth/logout', function(req, res, next) {
+  req.logout();
+  res.redirect('/');
 });
 
 app.start = function() {
@@ -142,11 +197,7 @@ app.start = function() {
   });
 };
 
-
-
-//flowtoUser.nestRemoting('RidAgency');
 // start the server if `$ node server.js`
 if (require.main === module) {
   app.start();
 }
-
