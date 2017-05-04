5911cd0ef366f2d7349b197966a7e6c958dff696
bird50 <siratis.w@gmail.com>
2017-05-05 06:03:51 +0700

    remove rename

--- server/server.js
+++ server/server.js
@@ -273,10 +273,10 @@ app.post('/photo/:container',function(req,res,next){
 	var storage =   multer.diskStorage({
 	  destination: function (req, file, callback) {
 	    callback(null, './upload_multer/'+req.params.container);
-	  },
-	  filename: function (req, file, callback) {
-	    callback(null, file.fieldname + '-' + Date.now());
 	  }
+	  //filename: function (req, file, callback) {
+	    //callback(null, file.fieldname + '-' + Date.now());
+	  //}
 	});
 
 	var upload = multer({ storage : storage}).single('file');
