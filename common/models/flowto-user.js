8e5bed256a36f144f8374df721134eb35c433b89
bird50 <siratis.w@gmail.com>
2017-05-10 12:41:56 +0700

    avatar

--- common/models/flowto-user.js
+++ common/models/flowto-user.js
@@ -1,9 +1,15 @@
 'use strict';
 
 module.exports = function(Flowtouser) {
+/*
 Flowtouser.afterSave = function(next) {
 	this.avatar=this.username;
 	console.log('save avatar by:'+this.avatar);
+	next();
+}
+	*/
+Flowtouser.beforeCreate = function(next, modelInstance) {
+	modelInstance.avatar=modelInstance.username[0];
 	next();
 }
 };
