a3f27f6479225740d390cf28425cf02add0b4db3
bird50 <siratis.w@gmail.com>
2017-08-04 07:56:37 +0700

    save account profile

--- server/server.js
+++ server/server.js
@@ -374,8 +374,9 @@ app.get('/rid_gmail_login', function(req, res, next) {
 				//console.log(error_ridgmail);
 				return res.status(404).send({error:"gmail นี้ยังไม่ได้ลงทะเบียนในระบบ ..."});
 			}else{
-				console.log('response_ridgmail:'+JSON.stringify(response_ridgmail));
+				
 				console.log('body_ridgmail:'+JSON.stringify(body_ridgmail));
+				console.log('body_ridgmail Account:'+body_ridgmail.Account);
 				var newUser = {};
 				newUser.email=body_obj.email;
 				newUser.username=body_obj.email.substring(0, body_obj.email.lastIndexOf("@"));//body_obj.name; 
